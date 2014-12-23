566aaf673d75d5dec695b688d67a0789c817b8d5
Naveen Raju <naveenraju9030@gmail.com>
2014-12-23 18:23:51 +0530

    program for object

---
+++ ojects1.js
@@ -0,0 +1,23 @@
+var object1={};
+object1["name"]="useni";
+object1["number"]="2222222";
+object1["age"]=23;
+console.log(object1);
+
+
+// object2 for sales
+var object2 ={};
+object2["product"]="jens";
+object2["quantity"]="22";
+object2["price"]=100;
+object2["discountPercentage"]=10;
+object2["total amount"]=2000;
+console.log(object2);
+// object3 for mobile recharge
+
+var object3={};
+object3["mobile_number"]=66666666;
+object3["plan"]="unlimited data";
+object3["amount"]=222;
+
+console.log(object3)
